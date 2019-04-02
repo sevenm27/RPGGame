874cc31787d0db5cd690f0740758791b13333997
SUSSEXDOWNS\480630 <480630@chrome.sussexdowns.ac.uk>
2019-04-02 12:52:03 +0100

    I added character animation.

---
+++ RPG/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	const float locomationAnimationSmoothTime = .1f;
+
+	NavMeshAgent agent;
+	Animator animator;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		animator = GetComponentInChildren<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float speedPercent = agent.velocity.magnitude / agent.speed;
+		animator.SetFloat("speedPercent", speedPercent, locomationAnimationSmoothTime, Time.deltaTime);
+	}
+}
