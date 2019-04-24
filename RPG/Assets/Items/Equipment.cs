948e045755db7f251f57da32d1dadf5726c3e999
SUSSEXDOWNS\480630 <480630@chrome.sussexdowns.ac.uk>
2019-04-24 10:22:45 +0100

    equipment

---
+++ RPG/Assets/Items/Equipment.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
+
+		public class Equipment : Item { 
+}
